082c05ea9756f70d06051fde24d55ddb5635bae4
osherasfa <36601013+osherasfa@users.noreply.github.com>
2018-02-18 19:05:32 +0200

    Update myCode.cpp

--- myCode.cpp
+++ myCode.cpp
@@ -1,51 +1,64 @@
-#include <WinSock2.h>
-#include "Server.h"
 #include <exception>
 #include <iostream>
 #include <string>
 #include <thread>
 #include <mutex>
+#include <deque>
+
+using namespace std;
 
 deque<string> _messagesQueue;
 std::condition_variable _cvMessages;
 std::mutex _mMessages;
 
-void getMessageFromSocket(SOCKET clientSocket)
+std::condition_variable _cv;
+std::mutex _m;
+
+void getMessageFromSocket()
 {
 	std::unique_lock<std::mutex> lckMessages(_mMessages, defer_lock); // not locked yet..
-
+	string data = "test";
 	lckMessages.lock(); // locking
 	_messagesQueue.push_back(data);
-	lckMessages.unlock(); 
+	lckMessages.unlock();
 
 	_cvMessages.notify_all(); // updating all (updateMessageQueue continuing)
 
+	std::unique_lock<std::mutex> lck(_m);
+	_cv.wait(lck);
 	lckMessages.lock();
 	_messagesQueue.pop_back(); // clearing messageQueue
 	lckMessages.unlock();
 }
 
-void updateMessagesQueue(SOCKET sc, string user)
+void updateMessagesQueue(string user)
 {
 	string message;
 
 	while (true)
 	{
 		std::unique_lock<std::mutex> lckMessages(_mMessages); // locking
-		if (_messagesQueue.empty()) // if messageQueue empty
-			_cvMessages.wait(lckMessages); // wait for notify
+		_cvMessages.wait(lckMessages); // wait for notify
 		lckMessages.unlock();
 
 		lckMessages.lock();
 		message = _messagesQueue.front(); // getting message from front of messagesQueue
-		Helper::updateFile(PATH, message); // updating file...
 		lckMessages.unlock();
+
+		_cv.notify_all();
 	}
 }
 
 
 void main()
 {
-	std::thread t1(updateMessagesQueue, someSocket, "user");
-	getMessageFromSocket(someSocket);
+	int n = 0;
+
+	while (n < 3)
+	{
+		std::thread t1(updateMessagesQueue, "user");
+		getMessageFromSocket();
+		t1.detach();
+		n++;
+	}
 }
